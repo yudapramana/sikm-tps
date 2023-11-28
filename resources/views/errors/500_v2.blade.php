fd936c45a954ef6da8a8d641d4fe7ae74d56c462
pramana yuda <pramanayuda772@gmail.com>
2023-11-28 09:52:02 +0700

    500v2

---
+++ resources/views/errors/500_v2.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!-- saved from url=(0040)https://vittoria.id.domainesia.com:2083/ -->
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>500 Internal Server Error</title>
+    <style type="text/css">
+        .errortype {
+            font-family: verdana, arial, helvetica, clean, sans-serif;
+            font-size: x-large;
+            font-weight: bold;
+        }
+
+        .errormessage {
+            font-family: verdana, arial, helvetica, clean, sans-serif;
+            font-size: large;
+            whitespace: pre;
+            margin-left: 20px;
+        }
+
+        p.errormessage {
+            color: #ff0000;
+        }
+
+        .serverinfo {
+            font-family: verdana, arial, helvetica, clean, sans-serif;
+            font-size: medium;
+        }
+    </style>
+</head>
+
+<body>
+    <h1 class="errortype">Internal Server Error</h1>
+    <p class="errormessage">500</p>
+    <pre>Error ID d63cec22f1bd</pre>
+    <p></p>
+    <hr>
+    <address class="serverinfo">cpsrvd Server at vittoria.id.domainesia.com</address>
+
+
+</body>
+
+</html>
