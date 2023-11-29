a38c2c06879e9d098fe454d660b801759331d184
pramana yuda <pramanayuda772@gmail.com>
2023-11-29 11:49:09 +0700

    push login

--- app/Http/Controllers/API/LoginController.php
+++ app/Http/Controllers/API/LoginController.php
@@ -48,7 +48,9 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        $user= User::where('email', $request->email)->first();
+        $user= User::where('email', $request->email)
+        ->orWhere('username', $request->email)
+        ->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
